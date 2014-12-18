e6dce6b8864b0cec111e55d843788ff11dc8b1fe
Sean Jensen-Grey <seanj@xyke.com>
2014-12-18 09:49:52 -0800

    added pattern matching
    Bugs could be introduced in the code via the Op and Item
    classes having the same members. The functional programming
    library from
    
    https://github.com/jdantonio/functional-ruby
    
    allows one to pattern match on the object type, removing an
    entire class of possible errors from the codebase.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,73 +1,84 @@
+# -*- immutable: string -*-
+
+
+require 'functional'
+
+Item = Struct.new(:name, :sell_in, :quality)
+
 class Op < Struct.new :item, :quality, :sell_in
   def initialize(item, quality=nil, sell_in=-1)
     super
   end
 end
 
-def quality_clamp(item)
-  if item.quality < 0
-    item.quality = 0
-  end
-  if item.quality > 50
-    item.quality = 50
-  end
-  item
-end
+class Object
+  include Functional::PatternMatching
+  private
+  defn(:quality_clamp, Item) { |item|
+    if item.quality < 0
+      item.quality = 0
+    end
+    if item.quality > 50
+      item.quality = 50
+    end
+    item
+  }
 
-def regular(item)
-  op = Op.new(item)
-  if item.sell_in <= 0
-    op.quality = -2
-  else
-    op.quality = -1
-  end
-  op
-end
+  defn(:regular, Item) { |item|
+    op = Op.new(item)
+    if item.sell_in <= 0
+      op.quality = -2
+    else
+      op.quality = -1
+    end
+    op
+  }
 
-def brie(op)
-  op.quality *= -1
-  op
-end
+  defn(:brie, Op) { |op|
+    op.quality *= -1
+    op
+  }
 
-def backstage(item)
-  op = Op.new(item)
-  case item.sell_in
-    when 11..Float::INFINITY
-      op.quality = 1
-    when 6..10
-      op.quality = 2
-    when 1..5
-      op.quality = 3
-    when -Float::INFINITY..0
-      op.quality = 0
-  end
-  op
-end
+  defn(:backstage, Item) { |item|
+    op = Op.new(item)
+    case item.sell_in
+      when 11..Float::INFINITY
+        op.quality = 1
+      when 6..10
+        op.quality = 2
+      when 1..5
+        op.quality = 3
+      when -Float::INFINITY..0
+        op.quality = 0
+    end
+    op
+  }
 
-def sulfuras(item)
-  op = Op.new(item)
-  item.quality = 80
-  op.quality = nil
-  op.sell_in = nil
-  op
-end
+  defn(:sulfuras, Item) { |item|
+    op = Op.new(item)
+    item.quality = 80
+    op.quality = nil
+    op.sell_in = nil
+    op
+  }
 
-def conjured(op)
-  if /Conjured/.match(op.item.name)
-    op.quality *= 2
-  end
-  op
-end
+  defn(:conjured, Op) { |op|
+    if /Conjured/.match(op.item.name)
+      op.quality *= 2
+    end
+    op
+  }
 
-def apply_op(op)
-  item = op.item
-  unless op.quality.nil?
-    item.quality += op.quality
-  end
-  unless op.sell_in.nil?
-    item.sell_in += op.sell_in
-  end
-  item
+  defn(:apply_op, Op) { |op|
+    item = op.item
+    unless op.quality.nil?
+      item.quality += op.quality
+    end
+    unless op.sell_in.nil?
+      item.sell_in += op.sell_in
+    end
+    item
+  }
 end
 
 
@@ -85,7 +96,3 @@ def update_quality(items)
     end
   end
 end
-
-######### DO NOT CHANGE BELOW #########
-
-Item = Struct.new(:name, :sell_in, :quality)
