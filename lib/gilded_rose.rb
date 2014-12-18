cb9b8ef03ea6b3b01fb4916d0fd460b6b8a921e5
Sean Jensen-Grey <seanj@xyke.com>
2014-12-18 09:45:22 -0800

    semifunctional solution
    Some things in the problem are under specified:
    
    * What can and cannot be Conjured
    * Negative sell_in values
    
    I took a liberal approach and made what I could
    conjured and support negative sell_in values because
    they log how long and item has been forsale.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,47 +1,87 @@
+class Op < Struct.new :item, :quality, :sell_in
+  def initialize(item, quality=nil, sell_in=-1)
+    super
+  end
+end
+
+def quality_clamp(item)
+  if item.quality < 0
+    item.quality = 0
+  end
+  if item.quality > 50
+    item.quality = 50
+  end
+  item
+end
+
+def regular(item)
+  op = Op.new(item)
+  if item.sell_in <= 0
+    op.quality = -2
+  else
+    op.quality = -1
+  end
+  op
+end
+
+def brie(op)
+  op.quality *= -1
+  op
+end
+
+def backstage(item)
+  op = Op.new(item)
+  case item.sell_in
+    when 11..Float::INFINITY
+      op.quality = 1
+    when 6..10
+      op.quality = 2
+    when 1..5
+      op.quality = 3
+    when -Float::INFINITY..0
+      op.quality = 0
+  end
+  op
+end
+
+def sulfuras(item)
+  op = Op.new(item)
+  item.quality = 80
+  op.quality = nil
+  op.sell_in = nil
+  op
+end
+
+def conjured(op)
+  if /Conjured/.match(op.item.name)
+    op.quality *= 2
+  end
+  op
+end
+
+def apply_op(op)
+  item = op.item
+  unless op.quality.nil?
+    item.quality += op.quality
+  end
+  unless op.sell_in.nil?
+    item.sell_in += op.sell_in
+  end
+  item
+end
+
+
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
+    case item.name
+      when /Aged Brie/
+        quality_clamp apply_op conjured brie regular item
+      when /Backstage passes to TAFKAL80ETC concert/
+        quality_clamp apply_op conjured backstage item
+      when 'Sulfuras, Hand of Ragnaros'
+        apply_op sulfuras item
       else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+        quality_clamp apply_op conjured regular item
     end
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -11,9 +11,111 @@ describe "#update_quality" do
     before { update_quality([item]) }
 
     it "your specs here" do
-      pending
+      item.sell_in.should == 4
+      item.quality.should == 9
     end
   end
+
+  context "sell by date passing" do
+    let(:item) { Item.new("normal item", 0, 10) }
+
+    before { update_quality([item]) }
+
+    it "sell by date, 2x quality drop" do
+      item.quality.should == 8
+    end
+  end
+
+  context "conjured sell by date" do
+    let(:item) { Item.new("Conjured Sword of Sell by Date", 0, 10) }
+
+    before { update_quality([item]) }
+
+    it "sell by date, 4x quality drop" do
+      item.quality.should == 6
+    end
+  end
+
+
+  context "sell by date passing, quality 0" do
+    let(:item) { Item.new("normal item", 0, 0) }
+
+    before { update_quality([item]) }
+
+    it "sell by date, quality 0" do
+      item.quality.should == 0
+    end
+  end
+
+
+  context "Aged Brie" do
+    let(:item) { Item.new("Aged Brie", 10, 10) }
+    before { update_quality([item]) }
+
+    it "default increment" do
+      item.sell_in.should == 9
+      item.quality.should == 11
+    end
+  end
+
+  context "Conjured Aged Brie" do
+    let(:item) { Item.new("Conjured Aged Brie", 10, 10) }
+    before { update_quality([item]) }
+
+    it "conjured default increment" do
+      item.sell_in.should == 9
+      item.quality.should == 12
+    end
+  end
+
+  context "aged brie @ 50" do
+    let(:item) { Item.new("Aged Brie", 10, 50) }
+    before { update_quality([item]) }
+
+    it "Aged Brie increment" do
+      item.sell_in.should == 9
+      item.quality.should == 50
+    end
+  end
+
+  context "backstage passes" do
+    let(:item) { Item.new("Backstage passes to TAFKAL80ETC concert", 12, 20) }
+
+    before { update_quality([item]) }
+
+    it "backstage passes" do
+      item.quality.should == 21
+      update_quality([item])
+      item.quality.should == 22
+      update_quality([item])
+      item.sell_in.should == 9
+      item.quality.should == 24
+
+    end
+  end
+
+  context "backstage passes day 10" do
+    let(:item) { Item.new("Backstage passes to TAFKAL80ETC concert", 10, 20) }
+
+    before { update_quality([item]) }
+
+    it "backstage passes" do
+      item.sell_in.should == 9
+      item.quality.should == 22
+    end
+  end
+
+  context "conjured backstage passes day 10" do
+    let(:item) { Item.new("Conjured Backstage passes to TAFKAL80ETC concert", 10, 20) }
+
+    before { update_quality([item]) }
+
+    it "backstage passes" do
+      item.sell_in.should == 9
+      item.quality.should == 24
+    end
+  end
+
 
   context "with multiple items" do
     let(:items) {
@@ -26,7 +128,8 @@ describe "#update_quality" do
     before { update_quality(items) }
 
     it "your specs here" do
-      pending
+      items[0].sell_in.should == 4
+      items[0].quality.should == 9
     end
   end
 end
